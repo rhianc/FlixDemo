d4c432713a32ea593e19a68615f71268984d11f7
Rhian Chavez <rhianchavez11@fb.com>
2017-06-22 21:26:43 -0700

    adding nav bar

---
+++ FlixDemo/CollectionViewController.swift
@@ -0,0 +1,45 @@
+//
+//  CollectionViewController.swift
+//  FlixDemo
+//
+//  Created by Rhian Chavez on 6/22/17.
+//  Copyright © 2017 Rhian Chavez. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FlixDemo/MovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.swift
+//  FlixDemo
+//
+//  Created by Rhian Chavez on 6/22/17.
+//  Copyright © 2017 Rhian Chavez. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
